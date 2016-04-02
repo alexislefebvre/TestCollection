949e39419f5062b2d0d0c3a70990ba4c3fd804cb
Alexis Lefebvre <alexislefebvre+github@gmail.com>
2016-04-02 19:59:43 +0200

    Add example with a simpler syntax

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -146,4 +146,63 @@ class DefaultControllerTest extends WebTestCase
             $inputs->eq(2)->attr('value')
         );
     }
+
+    public function testFormGetValuesAndPost()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/task');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $formTag = $crawler->filter('form');
+
+        $this->assertEquals(
+            2,
+            $formTag->filter('ul > li')->count()
+        );
+
+        ///// Get the form and set values
+        $form = $crawler->selectButton('Save')->form();
+
+        $values = $form->getPhpValues();
+
+        // Add value to collection (a tag).
+        $values['task']['tags'][]['name'] = 'tag3';
+
+        $crawler = $client->request($form->getMethod(), $form->getUri(), $values, $form->getPhpFiles());
+
+        $this->assertEquals(
+            1,
+            $crawler->filter('div.flash-notice')->count()
+        );
+        $this->assertContains(
+            'Your changes were saved!',
+            $crawler->filter('div.flash-notice')->text()
+        );
+
+        $this->assertEquals(
+            3,
+            $crawler->filter('ul > li')->count()
+        );
+
+        // One value (Tag) has been added.
+        $inputs = $crawler->filter('ul > li input[type="text"]');
+        $this->assertEquals(
+            3,
+            $inputs->count()
+        );
+        $this->assertEquals(
+            'tag1',
+            $inputs->eq(0)->attr('value')
+        );
+        $this->assertEquals(
+            'tag2',
+            $inputs->eq(1)->attr('value')
+        );
+        $this->assertEquals(
+            'tag3',
+            $inputs->eq(2)->attr('value')
+        );
+    }
 }
